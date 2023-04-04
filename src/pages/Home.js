bceaa7f2295927c4699375d75d38d16cf0146281
chester <jhunchesterlalongisip@gmail.com>
2023-04-04 13:28:14 +0800

    Create Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import '../css/Home.css';
 
 const Home = () => {
   return (
     <div className="Home">
+        <div className="content">
+            <h1>WELCOME</h1>
+            <h1>TO</h1>
+            <h1>GORDON COLLEGE ONLINE EVENT</h1>
+            <h1>REQUEST PORTAL</h1>
+        </div>
     </div>
   )
 }
